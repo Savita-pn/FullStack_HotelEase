44f94909fa5e363e1aa2cbf6160153f973df60d0
Savita P N <nekarsavita@gmail.com>
2025-10-16 17:19:37 +0530

    fix: Add missing SidebarProvider import in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
+import { SidebarProvider } from './context/SidebarContext';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import Home from './pages/Home';
